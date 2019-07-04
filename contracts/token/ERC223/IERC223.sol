27ccaf499463d546c69b8b5e481546ac4e6df436
Sparke2 <ligreat875@gmail.com>
2019-07-04 11:31:26 +0400

    Propose ERC223 token to openzeppelin solidity

---
+++ contracts/token/ERC223/IERC223.sol
@@ -0,0 +1,10 @@
+    
+pragma solidity ^0.5.0;
+
+contract IERC223 {
+    uint public totalSupply;
+    function balanceOf(address who) public view returns (uint);
+    function transfer(address to, uint value) public returns (bool success);
+    function transfer(address to, uint value, bytes memory data) public returns (bool success);
+    event Transfer(address indexed from, address indexed to, uint value, bytes data);
+}
